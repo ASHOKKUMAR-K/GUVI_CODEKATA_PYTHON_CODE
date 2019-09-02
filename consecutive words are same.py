591acd142927fb0fe9788340a9292d4637394eef
ASHOKKUMAR-K <41292977+ASHOKKUMAR-K@users.noreply.github.com>
2019-09-02 17:46:55 +0530

    Create consecutive words are same.py

---
+++ consecutive words are same.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = []
+for i in range(n):
+    a.append(str(input()))
+print(a)
+flag = False
+for i in range(n-1):
+	if a[i] == a[i+1]:
+    	flag = True
+if flag:
+	print("yes")
+else:
+    print("no")
