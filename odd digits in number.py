e4c5eacfa16e0cffd05167229587a6115d27b28c
ASHOKKUMAR-K <41292977+ASHOKKUMAR-K@users.noreply.github.com>
2019-09-04 15:14:49 +0530

    Create odd digits in number.py

---
+++ odd digits in number.py
@@ -0,0 +1,12 @@
+n = int(input())
+a=[]
+l = []
+while n>0:
+    d = n%10
+    l.insert(0, d)
+    n = n // 10
+for i in l:
+    if i % 2 != 0:
+        a.append(i)
+for i in a:
+    print(i, end=" ")
