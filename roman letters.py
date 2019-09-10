d08c4a48f7776064c0ed5e3ace856f631a554ad4
ASHOK KUMAR K <41292977+ASHOKKUMAR-K@users.noreply.github.com>
2019-09-10 10:08:11 +0530

    Create roman letters.py

---
+++ roman letters.py
@@ -0,0 +1,3 @@
+roman = {'I':1, 'II':2, 'III':3, 'IV':4, 'V':5, 'VI':6, 'VII':7, 'VIII':8, 'IX':9, 'X': 10, 'XI':11, 'XII':12, 'XIII':13, 'XIV':14, 'XV':15, 'XVI':16, 'XVII':17, 'XVIII':18, 'XIX':19, 'XX': 20}
+s = str(input())
+print(roman[s])
