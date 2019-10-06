d5d8a0b601beb28f5a86441ecfe19dbbff7b1586
ASHOK KUMAR K <41292977+ASHOKKUMAR-K@users.noreply.github.com>
2019-10-06 15:20:56 +0530

    Create largest common prefix - companies.py

---
+++ largest common prefix - companies.py
@@ -0,0 +1,12 @@
+n = int(input())
+l=[]
+for i in range(n):
+    l.append(str(input()))
+for i in range(len(l)):
+    flag = True
+    for j in l:
+        if l[0][i]!=j[i]:
+            flag = False
+            break
+    if flag:
+        print(l[0][i], end = "")
