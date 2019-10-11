c43d25db87f8b4c548e0bb01a7df8188f0bd6687
ASHOK KUMAR K <41292977+ASHOKKUMAR-K@users.noreply.github.com>
2019-10-11 19:34:14 +0530

    Create find the smallest number divisible by all the elements of the array.py

---
+++ find the smallest number divisible by all the elements of the array.py
@@ -0,0 +1,14 @@
+n = int(input())
+l = [int(x) for x in input().split()]
+i = max(l)
+while True:
+    flag = True
+    for j in l:
+        if i%j!=0:
+            flag = False
+            break
+    if flag:
+        print(i)
+        break
+    else:
+        i+=1
