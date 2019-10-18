a3fe375db8e6bf2613d66536e78613a9d21566e5
ASHOK KUMAR K <41292977+ASHOKKUMAR-K@users.noreply.github.com>
2019-10-18 18:25:41 +0530

    Update smallest number in the given list after each input.py

--- smallest number in the given list after each input.py
+++ smallest number in the given list after each input.py
@@ -1,7 +1,9 @@
 n = int(input())
 l = [int(x) for x in input().split()]
 a = []
+b = []
 for i in range(n):
   for j in range(i+1):
     a.append(l[j])
-  print(min(a), end = " ")
+  b.append(min(a))
+print(*b)
